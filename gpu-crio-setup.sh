d8fcbb0654684e0d256b89cab77362d207fa8cf6
Ali Khabazian <52274611+alikhabazian@users.noreply.github.com>
2025-09-28 09:47:41 +0330

    Update gpu-crio-setup.sh

--- gpu-crio-setup.sh
+++ gpu-crio-setup.sh
@@ -63,7 +63,8 @@ install_nvidia_toolkit(){
   log "Add nvidia key:"
   curl --proxy http://127.0.0.1:8118 -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit.gpg
   
-  distribution=$(. /etc/os-release; echo $ID$VERSION_ID)
+  #distribution=$(. /etc/os-release; echo $ID$VERSION_ID)
+  distribution="ubuntu20.04"
   curl --proxy http://127.0.0.1:8118 -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit.gpg] https://#g' | \
     sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
